21d2b56d5be4b7cfa92ff7d3639dc083f89857ba
Dheeraj Giri <dg598268@gmail.com>
2022-03-26 12:41:51 +0530

    first commite

---
+++ ip_address.py
@@ -0,0 +1,18 @@
+import socket as s
+
+
+def ip_address():
+
+    inp = input("Do you want system IP Address ? (y/n) ")
+
+    if inp in ["y", "Y"]:
+        print("Your Host name is ", s.gethostname())
+        print("IP Address ",s.gethostbyname(s.gethostname()))
+    else:
+        inp = input("Enter the URL of site ")
+        ip = s.gethostbyname(inp)
+        print("IP Address ", ip)
+    
+if __name__=="__main__":
+    ip_address()
+
